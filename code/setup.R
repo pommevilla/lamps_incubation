5a48f877cca8a1b9e4cdb8c43d6c4b048c53bad2
pommevilla <pvillanueva13@gmail.com>
2022-11-21 22:00:05 -0600

    Move setup

---
+++ code/setup.R
@@ -0,0 +1,67 @@
+library(tidyverse)
+library(lubridate)
+library(janitor)
+
+theme_set(
+  theme_light() +
+    theme(
+      panel.grid = element_blank(),
+      axis.line = element_line(color = "black", size = 0.5)
+    )
+)
+
+nice_expansion <- expansion(add = 0, mult = c(0, 0.1))
+
+data.priming <- read.csv("data/priming_amoA_deltaCt.csv", header = T) %>%
+  rename(sample_id = X) %>%
+  mutate(fert_level = as.factor(fert_level))
+
+data.raw <- read.csv("data/priming_amoA_rawCt.csv", header = T) %>%
+  rename(sample_id = X) %>%
+  mutate(fert_level = as.factor(fert_level))
+
+data.priming.long <- data.priming %>%
+  pivot_longer(cols = starts_with("amoA"), names_to = "amoA", values_to = "deltaCT")
+
+data.raw.long <- data.raw %>%
+  pivot_longer(cols = starts_with("amoA"), names_to = "amoA", values_to = "CT")
+
+data.priming.long$sample_id <- fct_reorder(data.priming.long$sample_id, parse_number(data.priming.long$sample_id))
+
+df <- data.priming[, -1]
+rownames(df) <- data.priming[, 1]
+
+metadata <- df %>%
+  select(fert_level:field_rep) %>%
+  mutate(across(everything(), as.factor))
+
+
+amoa_counts <- df %>%
+  select(starts_with("amoA"))
+
+n2o <- readr::read_csv("data/N2O.csv") %>%
+  select(-1) %>%
+  mutate(crop_within_block = interaction(crop, rep)) %>%
+  mutate(field_sample = interaction(crop, fert, rep)) %>%
+  mutate(addition = case_when(
+    addition == "Cntrl" ~ "Control",
+    addition == "+C" ~ "+ Carbon",
+    TRUE ~ "+ Nitrogen"
+  ))
+
+co2 <- read.csv("data/priming_calc.csv")
+
+tgas <- read.csv("data/tgas1.csv")
+
+# Colors
+addition_colors <- met.brewer("Benedictus", 3)
+names(addition_colors) <- levels(data.priming$addition)
+
+fertilization_colors <- met.brewer("VanGogh3", 2)
+names(fertilization_colors) <- levels(data.priming$fert_level)
+
+crop_colors <- met.brewer("Java", 2, direction = -1)
+names(crop_colors) <- levels(data.priming$crop)
+
+# To make a consistent x-axis for days
+day_breaks <- c(0, 4, 15, 30, 59, 86, 113, 144)

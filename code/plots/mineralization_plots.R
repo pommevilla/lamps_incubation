6ff0164d5c2377759cd1a63c0507c41c58e1febc
pommevilla <pvillanueva13@gmail.com>
2023-05-17 10:06:49 -0500

    Update labels, add abs/rel/nitr

--- code/plots/mineralization_plots.R
+++ code/plots/mineralization_plots.R
@@ -6,8 +6,6 @@
 ################## Setup
 source("code/setup.R")
 
-ammonium_label <- "$NH_4$"
-
 # Read in data
 mineralization_data <- read.csv(here("data/prepped_data", "mineralization_data.csv"))
 
@@ -48,29 +46,25 @@ plot_mineralization <- function(voi, palette, net_min_var, label, zero_line = FA
     theme(
       panel.border = element_rect(color = "black", fill = NA),
       plot.title = element_text(hjust = 0.5),
+      plot.subtitle = element_text(hjust = 0.5),
       aspect.ratio = 1
     ) +
     labs(
-      y = "Net minieralization",
-      x = "Day of experiment",
+      x = "",
+      y = "",
       fill = label,
       shape = label
     )
-
-
 
   return(p)
 }
 
 # Plot of net mineralization against factors
-net_mineralization_plot <- plot_mineralization(crop, crop_colors, net_mineralization_1, label = "Crop") +
-  plot_mineralization(addition, addition_colors, net_mineralization_1, label = "Addition") +
-  plot_mineralization(treatment, fertilization_colors, net_mineralization_1, label = "Fertilization Level")
+net_mineralization_plot <- plot_mineralization(crop, crop_colors, net_min_rate_rel, label = "Crop") +
+  plot_mineralization(addition, addition_colors, net_min_rate_rel, label = "Addition") +
+  plot_mineralization(treatment, fertilization_colors, net_min_rate_rel, label = "Fertilization Level")
 
-net_mineralization_plot +
-  theme(
-    text = element_text(family = "Times New Roman")
-  )
+net_mineralization_plot
 
 ggsave(
   "figures/net_mineralization.png"
@@ -86,91 +80,145 @@ ggsave(
 
 # The next plot will show nitrate, ammonium, and net mineralization broken down by
 # factors. The rows will be the factors and the columns will be nitrate, ammonium, and
-# net m. We start by building up the rows:
-crop_row_plots <- (plot_mineralization(crop, crop_colors, no3n_mg_kg_1, label = "Crop") +
-  theme(legend.position = "none") +
-  labs(
-    x = "",
-    y = "NO3",
-    title = "NO3"
-  )
-) +
-  (
-    plot_mineralization(crop, crop_colors, nh4n_mg_kg_1, label = "Crop") +
-      theme(legend.position = "none") +
-      labs(
-        x = "",
-        y = "NH4",
-        title = "NH4"
-      )
-  ) +
-  (
-    plot_mineralization(crop, crop_colors, net_mineralization_1, label = "Crop", zero_line = TRUE) +
-      labs(
-        x = "",
-        y = "Net mineralization",
-        title = "Net mineralization"
-      )
-  )
+# net m rate.
+# Helper function to plot individual rows nicely.
+# TODO: Figure out why purrr::map doesn't want to cooperate with this entire function
+plot_nnn_row <- function(voi, palette, label, titles = FALSE, xaxis = FALSE) {
+  p1 <- plot_mineralization({{ voi }}, palette, no3n_mg_kg_1, label = label, zero_line = TRUE) +
+    theme(
+      legend.position = "none",
+      plot.title = element_markdown(),
+      plot.subtitle = element_markdown()
+    )
 
-addition_row_plots <- (plot_mineralization(addition, addition_colors, no3n_mg_kg_1, label = "Addition") +
-  theme(legend.position = "none") +
-  labs(
-    x = "",
-    y = "NO3",
-  )
-) +
-  (
-    plot_mineralization(addition, addition_colors, nh4n_mg_kg_1, label = "Addition") +
-      theme(
-        legend.position = "none",
-        axis.title.x = element_markdown()
+  p2 <- plot_mineralization({{ voi }}, palette, nh4n_mg_kg_1, label = label, zero_line = TRUE) +
+    theme(
+      legend.position = "none",
+      plot.title = element_markdown(),
+      plot.subtitle = element_markdown()
+    )
+
+  p3 <- plot_mineralization({{ voi }}, palette, net_min_rate_rel, label = label, zero_line = TRUE)
+
+
+  if (titles) {
+    p1 <- p1 +
+      labs(
+        title = nitrate_label,
+        subtitle = flux_units
+      )
+    p2 <- p2 +
+      labs(
+        title = ammonia_label,
+        subtitle = flux_units
+      )
+    p3 <- p3 +
+      labs(
+        title = "Net mineralization rate (rel.)",
+        subtitle = paste(flux_units, per_day_unit)
       ) +
-      labs(
-        x = "",
-        y = "NH4",
+      theme(
+        plot.subtitle = element_markdown()
       )
-  ) +
-  (
-    plot_mineralization(addition, addition_colors, net_mineralization_1, label = "Addition", zero_line = TRUE) +
-      labs(
-        x = "",
-        y = "Net mineralization",
-      )
-  )
+  }
 
+  if (xaxis) {
+    p1 <- p1 + labs(x = "Day")
+    p2 <- p2 + labs(x = "Day")
+    p3 <- p3 + labs(x = "Day")
+  }
 
-treatment_row_plots <- (plot_mineralization(treatment, fertilization_colors, no3n_mg_kg_1, label = "N rate") +
-  theme(legend.position = "none") +
-  labs(
-    x = "Day",
-    y = "NO3",
-  )
-) +
-  (
-    plot_mineralization(treatment, fertilization_colors, nh4n_mg_kg_1, label = "N rate") +
-      theme(legend.position = "none") +
-      labs(
-        x = "Day",
-        y = "NH4",
-      )
+  p <- p1 + p2 + p3 + plot_layout(ncol = 3)
 
-  ) +
-  (
-    plot_mineralization(treatment, fertilization_colors, net_mineralization_1, label = "N rate", zero_line = TRUE) +
-      labs(
-        x = "Day",
-        y = "Net mineralization",
-      )
-  )
+  return(p)
+}
 
-# Combining the row plots together...
-crop_row_plots / addition_row_plots / treatment_row_plots
+# Putting rows together...
+wrap_plots(
+  plot_nnn_row(crop, crop_colors, "Crop", titles = TRUE),
+  plot_nnn_row(addition, addition_colors, "Addition"),
+  plot_nnn_row(treatment, fertilization_colors, "N Rate", xaxis = TRUE),
+  nrow = 3
+)
 
 # ...and saving them.
 ggsave(
-  "figures/nitrate_ammonium_net_m_by_factors.png",
-  width = 4000,
-  height = 4000,
+  "figures/mineralization/nitrate_ammonium_net_m_by_factors.png",
+  width = 3000,
+  height = 2900,
+  units = "px",
+)
+
+## Absolute and relative net mineralization and nitrification
+plot_armn_row <- function(voi, palette, label, titles = FALSE, xaxis = FALSE) {
+  p1 <- plot_mineralization({{ voi }}, palette, net_min_rate_rel, label = label, zero_line = TRUE) +
+    theme(
+      legend.position = "none",
+      plot.title = element_markdown(),
+      plot.subtitle = element_markdown()
+    )
+
+  p2 <- plot_mineralization({{ voi }}, palette, net_min_rate_abs, label = label, zero_line = TRUE) +
+    theme(
+      legend.position = "none",
+      plot.subtitle = element_markdown()
+    )
+
+  p3 <- plot_mineralization({{ voi }}, palette, net_nitr_rate_rel, label = label, zero_line = TRUE) +
+    theme(
+      legend.position = "none",
+      plot.subtitle = element_markdown()
+    )
+
+  p4 <- plot_mineralization({{ voi }}, palette, net_nitr_rate_abs, label = label, zero_line = TRUE) +
+    theme(
+      plot.subtitle = element_markdown()
+    )
+
+  # This sucks
+  # TODO: Figure out why purrr::map doesn't like this
+  if (titles) {
+    p1 <- p1 + labs(
+      title = "Net mineralization rate (rel.)",
+      subtitle = paste(flux_units, per_day_unit)
+    )
+    p2 <- p2 + labs(
+      title = "Net mineralization rate (abs.)",
+      subtitle = paste(flux_units, per_day_unit)
+    )
+    p3 <- p3 + labs(
+      title = "Net nitrification rate (rel.)",
+      subtitle = paste(flux_units, per_day_unit)
+    )
+    p4 <- p4 + labs(
+      title = "Net nitrification rate (abs.)",
+      subtitle = paste(flux_units, per_day_unit)
+    )
+  }
+
+  # So does this
+  if (xaxis) {
+    p1 <- p1 + labs(x = "Day")
+    p2 <- p2 + labs(x = "Day")
+    p3 <- p3 + labs(x = "Day")
+    p4 <- p4 + labs(x = "Day")
+  }
+
+  p <- p1 + p2 + p3 + p4 + plot_layout(ncol = 4)
+
+  return(p)
+}
+
+wrap_plots(
+  plot_armn_row(crop, crop_colors, "Crop", titles = TRUE),
+  plot_armn_row(addition, addition_colors, "Addition"),
+  plot_armn_row(treatment, fertilization_colors, "N rate", xaxis = TRUE),
+  nrow = 3
+)
+
+ggsave(
+  "figures/mineralization/net_min_nitr_abs_rel.png",
+  width = 4300,
+  height = 3000,
   units = "px",
 )

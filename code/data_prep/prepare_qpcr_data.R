12f5f2a052e908c045b4a917afd52d317e89f406
pommevilla <pvillanueva13@gmail.com>
2023-06-01 09:40:24 -0500

    Add norB data

--- code/data_prep/prepare_qpcr_data.R
+++ code/data_prep/prepare_qpcr_data.R
@@ -6,24 +6,50 @@
 ################## Setup
 source("code/setup/setup.R")
 
-qpcr_data <- read.csv(here("data/qpcr", "Incubation_Biomark-qPCR_all_20230525.csv")) %>%
-  mutate(Treatment = case_when(
-    Treatment == "100N" ~ "112N",
-    Treatment == "300N" ~ "336N",
-    TRUE ~ Treatment
-  )) %>%
+amoa_data <- read.csv(here("data/qpcr", "Incubation_Biomark-qPCR_all_20230525.csv")) %>%
+  mutate(
+    Crop = if_else(Crop == "Mxg", "Miscanthus", "Corn"),
+    ave_sum = ave_012 + ave_025 + ave_039,
+  ) %>%
+  rename(
+    Day = DNA_DOE,
+  ) %>%
+  # We drop this from here since it'll show up in the norB dataset.
+  select(-norB.001)
+
+# We keep Crop, Treatment, Addition, and Day to check that these columns
+# match between datasets.
+norb_data <- read.csv(
+  here::here("data/qpcr", "overall_norB_20230530.csv")
+) %>%
+  select(
+    Sample.Name, Crop, Treatment, Addition,
+    Day = DNA_DOE, norB.001, norB.006, cnorB
+  ) %>%
   mutate(
     Crop = if_else(
       Crop == "Mxg", "Miscanthus", "Corn"
     ),
-    log_norB = log10(norB.001),
-    ave_sum = ave_012 + ave_025 + ave_039,
-    log_sum = log(ave_sum)
-  ) %>%
-  rename(
-    Day = DNA_DOE,
-    ave_norB = norB.001
+    norb_sum = norB.001 + norB.006,
+    across(
+      contains("norB"),
+      log,
+      .names = "log_{.col}"
+    )
   )
+
+# After doing checks, we can drop the extra columns from above.
+qpcr_data <- left_join(
+  amoa_data,
+  norb_data %>%
+    select(-c(Crop, Treatment, Addition, Day)),
+  by = "Sample.Name"
+) %>%
+  mutate(Treatment = case_when(
+    Treatment == "100N" ~ "112N",
+    Treatment == "300N" ~ "336N",
+    TRUE ~ Treatment
+  ))
 
 write.csv(
   qpcr_data,
